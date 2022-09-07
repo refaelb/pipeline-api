644d63f2ed933a57d8bd92160cba9b27bd07fbec
Your Name <you@example.com>
2022-09-07 23:31:15 +0300

    api add the files

--- git_command.sh
+++ git_command.sh
@@ -7,7 +7,7 @@ project_name=$1
 token=$TOKEN
 azureuser=$ADMINUSER
 
-cd ./../$project_name
+cd ./../projects/$project_name
 git init .
 # git remote add origin   https://{$azureuser}:{$token}@dev.azure.com/yesodot/migration1/_git/migration1
 # git remote add origin   https://Ontario-135:4dks6lvuaq7plwo4g3vaphd4w2yxut6mf2o3d5dpbgaoywahczzq@dev.azure.com/yesodot/migration1/_git/migration1
