859d58a08e9b52f1259625085811b9d60c4aeb4e
refaelb <refael058325@gmail.com>
2022-09-07 16:10:10 +0300

    move to work in bus

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import json
 import yaml
 # import git 
 import os
+from pathlib import Path
 
 
-valuse="""
-name: {service_name}
+
+valuse="""name: {service_name}
 replicaCount: {service_replica_count}
 
 images:
@@ -21,10 +22,10 @@ service:
  ports: {service_port}
 
 volume:
-    enable: {volume_status}
-  - name: {volume_name}
+  - enable: {volume_status}
+    name: {volume_name}
     type: volumeClaim
-    claimName: {volume_name}"-claim"
+    claimName: {volume_name} 
     mounts:
       - mountPath: {volume_mount}
         subPath: {volume_path}
@@ -40,7 +41,7 @@ ingress:
   - host: {ingress_host}
     paths:
       - path: {ingress_path}
-        service: {service_name}
+        service: {ingress_service_name}
         port: 80
 """
 
@@ -68,7 +69,7 @@ releases:
 
 app = Flask(__name__)
 
-@app.route('/pipline_api/create', methods=['POST'])
+@app.route('/pipeline_api/create', methods=['POST'])
 def pipline_api():
     data = (request.data)
     res = validate(data)
@@ -89,27 +90,32 @@ def validate(data):
         service_name=(json_data["services"][service]["name"])
         service_tag=(json_data["services"][service]["tag"])
         service_repo=(json_data["services"][service]["repository"])
-        service_replica_count=(json_data["service"][service]["replicacount"])
-        service_port=(json_data["service"][service]["port"])
+        service_replica_count=(json_data["services"][service]["replicacount"])
+        service_port=(json_data["services"][service]["port"])
+        volume_status=(json_data["services"][service]["volumes"]["enable"])
         volume_name=(json_data["services"][service]["volumes"]["name"])
         volume_mount=(json_data["services"][service]["volumes"]["mount_path"])
         volume_sub=(json_data["services"][service]["volumes"]["sub_path"])
         ingress_status=(json_data["services"][service]["ingress"]["enable"])
         ingress_path=(json_data["services"][service]["ingress"]["path"])
         ingress_host=(json_data["services"][service]["ingress"]["host"])
-        write(project_name, service_name,pullSecrets, service_repo, service_tag, volume_mount, volume_sub, volume_name,configmap,ingress_path, ingress_status,ingress_host,service_port, service_replica_count)
+        ingress_service_name=(json_data["services"][service]["ingress"]["service_name"])
+        write(project_name, service_name,service_replica_count, pullSecrets, service_repo, service_tag,volume_status,volume_name, volume_mount, volume_sub,configmap,ingress_status,ingress_path, ingress_status,ingress_host,ingress_path,ingress_service_name)
     helmfile_write(project_name, azure_user)
-    return  project_name, service_name, service_repo, service_tag, volume_mount, volume_sub, volume_name,ingress_path, ingress_status,ingress_host,service_port, service_replica_count, pullSecrets,configmap
+    return  service_name,service_replica_count, pullSecrets, service_repo, service_tag,volume_status,volume_name, volume_mount, volume_sub,configmap,ingress_status,ingress_path, ingress_status,ingress_host,ingress_path,ingress_service_name
 
 ##create values.yaml files
-def write( project_name, service_name,pullSecrets, service_repo, service_tag, volume_mount, volume_sub, volume_name,configmap,ingress_path, ingress_status,ingress_host,service_port, service_replica_count):
-    with open ("./../"+project_name+"/valuse/"+service_name+"-valuse.yaml","a+") as f :
-        var_list = yaml.load(valuse, Loader=yaml.FullLoader)
-        yaml.dump(var_list, f.format( service_name,pullSecrets, service_repo, service_tag, volume_mount, volume_sub, volume_name,configmap,ingress_path, ingress_status,ingress_host,service_port, service_replica_count))
+def write( project_name,service_name,service_replica_count, pullSecrets, service_repo, service_tag,volume_status,volume_name, volume_mount, volume_sub,configmap,ingress_status,ingress_path,ingress_host,ingress_service_name):
+    # Path("./"+project_name+"/values").mkdir(parents=True, exist_ok=True)
+    if not os.path.exists("./"+project_name+"/values"):
+      os.makedirs("./"+project_name+"/values")
+    with open ("./"+project_name+"/values/"+service_name+"-values.yaml","a+") as f :
+        var_list = yaml.load(valuse.format(service_name,service_replica_count, pullSecrets, service_repo, service_tag,volume_status,volume_name,volume_name, volume_mount, volume_sub,configmap,ingress_status,ingress_host,ingress_path,ingress_service_name))
+        yaml.dump(var_list, f)
 
 ##create helmfile
 def helmfile_write(project_name, azure_user):
-    with open ("./../"+project_name+"/helmfile/helmfile.yaml","a+") as f :
+    with open ("./"+project_name+"/helmfile/helmfile.yaml","a+") as f :
         var_list = yaml.load(valuse, Loader=yaml.FullLoader)
         yaml.dump(var_list, f)
     maker(project_name, azure_user)
